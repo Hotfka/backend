43d3340c35a31338509e1165c31027655df6f8dc
jun <ggprgrkjh@naver.com>
2023-09-22 20:35:21 +0900

    feat: Message Entity

--- OpenChatServer/src/main/java/com/example/openchatserver/entity/Message.java
+++ OpenChatServer/src/main/java/com/example/openchatserver/entity/Message.java
@@ -1,14 +1,33 @@
 package com.example.openchatserver.entity;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.sql.Blob;
+
 @NoArgsConstructor
 @Getter
 @Entity
-public class Message {
+public class Message extends BaseDateEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name="message_id")
+    private Long id;
+
+    private String text;
+
+    private String sender;
+
+
+
 
 }
