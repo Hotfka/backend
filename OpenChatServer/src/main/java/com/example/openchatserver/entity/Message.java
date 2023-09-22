4a887422863352406cd1ed960bd4a3302a0b093c
jun <ggprgrkjh@naver.com>
2023-09-22 20:33:00 +0900

    feat: BaseDateEntity

---
+++ OpenChatServer/src/main/java/com/example/openchatserver/entity/Message.java
@@ -0,0 +1,14 @@
+package com.example.openchatserver.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Message {
+
+}
