c936659e60942e007a515e3fd566b1fa884b5f57
jun <ggprgrkjh@naver.com>
2023-09-22 18:06:18 +0900

    first setting

---
+++ OpenChatServer/src/main/java/com/example/openchatserver/OpenChatServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.openchatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenChatServerApplication.class, args);
+	}
+
+}
