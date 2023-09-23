48d7ec76f0bb38c25fedf98fbfa97388dbdeffe2
jun <ggprgrkjh@naver.com>
2023-09-23 17:16:33 +0900

    feat: OpenChatServer kafkaProducer sendMessageEvent

--- OpenChatServer/src/main/java/com/example/openchatserver/service/MessageService.java
+++ OpenChatServer/src/main/java/com/example/openchatserver/service/MessageService.java
@@ -15,13 +15,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final KafkaProducer kafkaProducer;
+
 
     @Transactional
     public SendMessageResponse sendMessage(SendMessageRequest request){
 
         Message message = new Message(request);
         messageRepository.save(message);
-
+        kafkaProducer.sendMessageEvent(message);
         SendMessageResponse sendMessageResponse = new SendMessageResponse(message);
 
         return sendMessageResponse;
