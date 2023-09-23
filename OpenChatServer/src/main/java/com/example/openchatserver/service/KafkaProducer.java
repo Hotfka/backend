083a5969b3ddc9786d42b99ab34aaa2a0c37d542
jun <ggprgrkjh@naver.com>
2023-09-23 17:13:55 +0900

    feat: OpenChatServer KafkaProducer

---
+++ OpenChatServer/src/main/java/com/example/openchatserver/service/KafkaProducer.java
@@ -0,0 +1,18 @@
+package com.example.openchatserver.service;
+
+import com.example.openchatserver.entity.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducer {
+    private static final String TOPIC = "sendMessage";
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    public void sendMessageEvent(Message message) {
+        System.out.println(String.format("Produce message : %s", message.getText()));
+        this.kafkaTemplate.send(TOPIC, message.getText());
+    }
+
+}
