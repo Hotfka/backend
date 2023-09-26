83e5905c05bdaf0306487cc54056107335213dcd
jun <ggprgrkjh@naver.com>
2023-09-26 14:55:58 +0900

    feat: ReactionController updateReaction

--- OpenChatServer/src/main/java/com/example/openchatserver/controller/ReactionController.java
+++ OpenChatServer/src/main/java/com/example/openchatserver/controller/ReactionController.java
@@ -1,11 +1,30 @@
 package com.example.openchatserver.controller;
 
 
+import com.example.openchatserver.dto.SendMessageRequest;
+import com.example.openchatserver.dto.SendMessageResponse;
+import com.example.openchatserver.service.MessageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ReactionController {
+
+
+    private final ReactionService reactionService;
+
+    @PutMapping("/api/v1/updateReaction")
+    public ResponseEntity<updateReactionResponse> updateReaction(@RequestParam(name="reaction")String reaction){
+
+        updateReactionResponse updateReactionResponse = reactionService.updateReaction();
+
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(updateReactionResponse);
+    }
+
 
 }
