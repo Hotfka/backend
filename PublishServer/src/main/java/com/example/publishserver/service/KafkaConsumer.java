68c621653a39700296b1ab0d67e54efc6a07069a
jun <ggprgrkjh@naver.com>
2023-09-27 17:23:06 +0900

    feat: KafkaConsumer consumeReaction

--- PublishServer/src/main/java/com/example/publishserver/service/KafkaConsumer.java
+++ PublishServer/src/main/java/com/example/publishserver/service/KafkaConsumer.java
@@ -13,7 +13,13 @@ public class KafkaConsumer {
     private final SSEController sseController;
 
     @KafkaListener(topics = "sendMessage", groupId = "hotfka")
-    public void consume(String messageUserName) throws IOException {
+    public void consumeMessage(String messageUserName) throws IOException {
+        System.out.println(String.format("Consumed message getUserName: %s", messageUserName));
+        sseController.sendData(messageUserName);
+    }
+
+    @KafkaListener(topics = "sendReaction", groupId = "hotfka")
+    public void consumeReaction(String messageUserName) throws IOException {
         System.out.println(String.format("Consumed message getUserName: %s", messageUserName));
         sseController.sendData(messageUserName);
     }
