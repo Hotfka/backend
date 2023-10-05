8c8275f1beabe5f4dabaf8bed1a59674ee3591d5
jun <ggprgrkjh@naver.com>
2023-10-05 21:07:53 +0900

    feat: redis throttling in messageService

--- OpenChatServer/src/main/java/com/example/openchatserver/service/MessageService.java
+++ OpenChatServer/src/main/java/com/example/openchatserver/service/MessageService.java
@@ -7,9 +7,12 @@ import com.example.openchatserver.dto.SendMessageResponse;
 import com.example.openchatserver.entity.Message;
 import com.example.openchatserver.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +24,7 @@ public class MessageService {
     private final MessageRepository messageRepository;
     private final KafkaProducer kafkaProducer;
 
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional
     public SendMessageResponse sendMessage(SendMessageRequest request){
@@ -36,6 +40,9 @@ public class MessageService {
     @Transactional
     public List<GetMessagesResponse> getMessages(){
 
+
+        redisTemplate.opsForValue().increment("event_count",1);
+
         List<Message> messages =  messageRepository.findAll();
 
         List<GetMessagesResponse> messagesResponses = messages.stream()
@@ -45,6 +52,24 @@ public class MessageService {
         return messagesResponses;
 
     }
+
+    @Scheduled(fixedRate = 1000)
+    public void checkHotChat() throws IOException {
+
+        Long currentEventCount = Long.valueOf(redisTemplate.opsForValue().get("event_count"));
+        System.out.println("redis count: "+currentEventCount + " hotchat : " + redisTemplate.opsForValue().get("hotchat"));
+
+        if (currentEventCount >= 700) {
+            redisTemplate.opsForValue().set("hotchat", "true");
+            System.out.println("hotchat created : "+currentEventCount);
+        } else {
+            // 700개 이하이면 hotchat 해제
+            redisTemplate.delete("hotchat");
+        }
+        redisTemplate.opsForValue().set("event_count", "0");
+    }
+
+
 
 
 
--- PublishServer/src/main/java/com/example/publishserver/service/KafkaConsumer.java
+++ PublishServer/src/main/java/com/example/publishserver/service/KafkaConsumer.java
@@ -3,6 +3,7 @@ import com.example.publishserver.controller.SSEController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -19,24 +20,13 @@ public class KafkaConsumer {
     public void consumeMessage(String messageUserName) throws IOException {
         System.out.println(String.format("Consumed message getUserName: %s", messageUserName));
         sseController.sendMessageEvent(messageUserName);
-        checkHotChat();
-
     }
 
     @KafkaListener(topics = "sendReaction", groupId = "hotfka")
     public void consumeReaction(String messageUserName) throws IOException {
         System.out.println(String.format("Consumed reaction getUserName: %s", messageUserName));
         sseController.sendReactionEvent(messageUserName);
-        checkHotChat();
     }
-
-    public void checkHotChat() throws IOException{
-        Long eventCount = redisTemplate.opsForValue().increment("event_count",1);
-        if(eventCount >= 700){
-            redisTemplate.opsForValue().set("hotchat","true");
-        }
-    }
-
 
 
 }
