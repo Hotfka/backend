53c2dc7e1a719abfafcfbceddf7ba454a35982f7
jun <ggprgrkjh@naver.com>
2023-10-05 20:17:33 +0900

    feat: checkHotChat method

--- PublishServer/src/main/java/com/example/publishserver/service/KafkaConsumer.java
+++ PublishServer/src/main/java/com/example/publishserver/service/KafkaConsumer.java
@@ -19,12 +19,24 @@ public class KafkaConsumer {
     public void consumeMessage(String messageUserName) throws IOException {
         System.out.println(String.format("Consumed message getUserName: %s", messageUserName));
         sseController.sendMessageEvent(messageUserName);
+        checkHotChat();
+
     }
 
     @KafkaListener(topics = "sendReaction", groupId = "hotfka")
     public void consumeReaction(String messageUserName) throws IOException {
         System.out.println(String.format("Consumed reaction getUserName: %s", messageUserName));
         sseController.sendReactionEvent(messageUserName);
+        checkHotChat();
     }
+
+    public void checkHotChat() throws IOException{
+        Long eventCount = redisTemplate.opsForValue().increment("event_count",1);
+        if(eventCount >= 700){
+            redisTemplate.opsForValue().set("hotchat","true");
+        }
+    }
+
+
 
 }
